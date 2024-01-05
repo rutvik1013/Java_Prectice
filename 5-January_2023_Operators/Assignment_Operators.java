b943d4d4ec5e6fafd8bc4cd1f52b83d1445b76ce
rutvik1013 <113574475+rutvik1013@users.noreply.github.com>
2024-01-05 12:01:45 +0530

    Add files via upload

---
+++ 5-January_2023_Operators/Assignment_Operators.java
@@ -0,0 +1,33 @@
+// Assignement Operators
+public class Assignment_Operators {
+    public static void main(String[] args) {
+
+        int a=8;
+        int b;
+
+        // 1. Assign value using=
+        b=a;
+        System.out.println("= "+b);
+
+        // 2. Assign Value using+=
+        b+=a;
+        System.out.println("+= "+b);
+
+        //3. Assign Value using -=
+        b-=a;
+        System.out.println("-= "+b);
+
+        //4. Assign Value using *=
+        b*=a;
+        System.out.println("*= "+b);
+
+        //5. Assign Value using /=
+        b/=a;
+        System.out.println("/= "+b);
+
+        //6. Assign Value using %=
+        b%=a;
+        System.out.println("%= "+b);
+
+    }
+}
---
+++ 5-January_2023_Operators/Unary_Operators.java
@@ -0,0 +1,17 @@
+//Unary Operators
+
+public class Unary_Operators {
+    public static void main(String[] args) {
+        int a=10;
+        int b=7;
+
+        //Postincreament and Preincreament
+        System.out.println("Postincreament: "+(a++));
+        System.out.println("Preincreament: "+(++a));
+
+        //PostDecreament and Predecreament
+        System.out.println("PostDecreament: "+(b--));
+        System.out.println("PreDecreament: "+(--b));
+
+    }
+}
