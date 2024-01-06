a9eb6c15233883b8f2d4d3113b713d324e13f368
rutvik1013 <113574475+rutvik1013@users.noreply.github.com>
2024-01-06 17:16:14 +0530

    Add files via upload

---
+++ 06-January-2023/Introduction_to_you.java
@@ -0,0 +1,14 @@
+//1. Display This Information using println
+//a. Your Name
+//b. Your Birth date
+//c. Your Age
+//d. Your Addres
+public class Introduction_to_you
+{
+    public static void main(String[] args) {
+        System.out.println("My Name is Babariya Rutvik");
+        System.out.println("My Birth Date is 24h March, 2000");
+        System.out.println("I Am 23 Years Old");
+        System.out.println("I Lived in Junagadh,Gujarat");
+    }
+}
