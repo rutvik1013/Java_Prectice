e782dd2f3f4fb4eab7ef71a70b2415c192e25835
rutvik1013 <113574475+rutvik1013@users.noreply.github.com>
2024-02-23 11:19:31 +0530

    Add files via upload

--- Java_Prectice/Constructors.java
+++ Java_Prectice/Constructors.java
@@ -1,14 +1,18 @@
-public class Constructors
-{
-    // Default Constructors
-    Constructors(){
+class name{
+
+    // Paramiterised Constructors
+    String surname;
+    String name;
+
+    name(String surname,String name){
+        this.surname=surname;
+        this.name=name;
+
+    }
+}
+public class Constructors {
+    public static void main(String[] args) {
+        name fullname=new name("Babariya","Rutvik");
         System.out.println("Babariya Rutvik");
     }
-
-    public static void main(String[] args) {
-        Constructors c=new Constructors();
-
-    }
-
-
 }
