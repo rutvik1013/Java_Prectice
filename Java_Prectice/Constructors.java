f8c2b1bdd22a6e79502a21519e502fc1a61d5b6c
rutvik1013 <113574475+rutvik1013@users.noreply.github.com>
2024-02-23 11:10:55 +0530

    Add files via upload

---
+++ Java_Prectice/Constructors.java
@@ -0,0 +1,14 @@
+public class Constructors
+{
+    // Default Constructors
+    Constructors(){
+        System.out.println("Babariya Rutvik");
+    }
+
+    public static void main(String[] args) {
+        Constructors c=new Constructors();
+
+    }
+
+
+}
