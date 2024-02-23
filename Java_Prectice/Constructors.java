4fa013d508cfc3ca571b6eb4ac0717689a70c3eb
rutvik1013 <113574475+rutvik1013@users.noreply.github.com>
2024-02-23 11:27:12 +0530

    Add files via upload

--- Java_Prectice/Constructors.java
+++ Java_Prectice/Constructors.java
@@ -1,18 +1,24 @@
-class name{
 
-    // Paramiterised Constructors
+// Copy Constructors
+class name_f{
     String surname;
     String name;
-
-    name(String surname,String name){
+    name_f(String surname,String name){
         this.surname=surname;
         this.name=name;
+    }
+    name_f(name_f n1){
+        this.surname=n1.surname;
+        this.name=n1.name;
 
     }
 }
 public class Constructors {
     public static void main(String[] args) {
-        name fullname=new name("Babariya","Rutvik");
+        name_f n1=new name_f("Babariya","Rutvik");
+        System.out.println("Babariya Rutvik");
+
+        name_f n2=new name_f("Babariya","Rutvik");
         System.out.println("Babariya Rutvik");
     }
 }
